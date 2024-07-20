30b18fe318c90fb5f3136f586954a9e9e1175385
Jose Miguel <jmvr.2310@gmail.com>
2024-07-20 16:49:14 -0400

    crispy forms

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,9 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
+    # third
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
+
+# django crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 MIDDLEWARE = [
