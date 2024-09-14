74ccc39c31da5fe98a37dcaf20ded55738fe8a1f
Jose Miguel <jmvr.2310@gmail.com>
2024-09-14 17:48:55 -0400

    search bar functionality

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 
 # Create your views here.
 class BookListView(ListView):
@@ -16,3 +17,9 @@ class BookDetailView(LoginRequiredMixin, DetailView):
 class SearchResultsView(ListView):
     model = Book
     template_name = 'books/search.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query)|Q(author__icontains=query)
+            )
