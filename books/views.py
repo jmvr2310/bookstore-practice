640a35cda15598c4d94f359a02ec514d25513383
Jose Miguel <jmvr.2310@gmail.com>
2024-09-14 16:43:18 -0400

    search result page

--- books/views.py
+++ books/views.py
@@ -12,3 +12,7 @@ class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     login_url = 'login'
+
+class SearchResultsView(ListView):
+    model = Book
+    template_name = 'books/search.html'
